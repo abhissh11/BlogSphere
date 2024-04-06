b59e6842063362563983f73e1614f179ae116110
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-04-06 13:23:39 +0530

    Implemented User Route && Auth Route in backend

---
+++ api/controllers/user-controller.js
@@ -0,0 +1,5 @@
+export const test =
+  ("/test",
+  (req, res) => {
+    res.json({ msg: "API Working" });
+  });
--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user-route.js";
+import authRouter from "./routes/auth-route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,4 +20,14 @@ mongoose
 
 app.listen(4000, () => {
   console.log(`server running on PORT 4000`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
