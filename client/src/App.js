8ed0df9375c19df0c52b4e1a527f4eef5d3f1965
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-04-15 13:09:44 +0530

    Update Post API route & UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import AdminPrivateRoute from "./components/AdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
         </Route>
         <Route element={<AdminPrivateRoute />}>
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
       </Routes>
       <Footer />
