c54ca8face438c62e5f842346cb6c460111a1e94
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-04-18 03:20:10 +0530

    About & Project page Updated

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,49 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About blogSphere
+            <span className="font-extralight text-sm"> - by Abhishek</span>
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to blogSphere, your premier destination for insightful
+              articles and captivating content!
+            </p>
+
+            <p>
+              At blogSphere, we are dedicated to curating a diverse collection
+              of blogs that cater to a wide range of interests, with a
+              particular focus on the dynamic realms of technology and
+              artificial intelligence (AI). Our platform serves as a haven for
+              tech enthusiasts, developers, and innovators alike, offering a
+              wealth of resources and expertise on topics such as the MERN
+              stack, Next.js, and cutting-edge advancements in the world of AI.
+            </p>
+
+            <p>
+              In addition to our robust tech-centric content, blogSphere also
+              features engaging blogs on various other subjects, including life,
+              movies, and web shows. Whether you're seeking practical advice on
+              mastering the latest programming languages, exploring the
+              intricacies of life's many facets, or discovering hidden gems in
+              the world of entertainment, you'll find it all right here.
+            </p>
+            <p>
+              Join us on this exciting journey of exploration, learning, and
+              discovery. Whether you're a seasoned professional or a curious
+              newcomer, there's always something new to discover at blogSphere.
+            </p>
+            <p>Thank you for being a part of our vibrant community! ❤ </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
